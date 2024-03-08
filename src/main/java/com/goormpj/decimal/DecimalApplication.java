f313e60f1c96f2888c2042b835356f3c789ff9c8
dawoon jung <daewangbali@gmail.com>
2024-03-09 00:59:11 +0900

    setup: initial project settings

---
+++ src/main/java/com/goormpj/decimal/DecimalApplication.java
@@ -0,0 +1,13 @@
+package com.goormpj.decimal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecimalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecimalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goormpj/decimal/DecimalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goormpj.decimal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecimalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
