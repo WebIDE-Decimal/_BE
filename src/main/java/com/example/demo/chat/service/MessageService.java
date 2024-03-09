2a10c3dbf18ceb2c46864d3eb498d53a78e724bd
Gom1031 <atheniald@gmail.com>
2024-03-09 09:23:10 +0900

    mvp midterm

--- src/main/java/com/example/demo/chat/service/MessageService.java
+++ src/main/java/com/example/demo/chat/service/MessageService.java
@@ -20,10 +20,10 @@ public class MessageService {
     // 메시지 전송
     public ChatMessage sendMessage(Long roomId, Long userId, String messageContent) {
         ChatMessage message = new ChatMessage();
-        message.setType(MessageType.CHAT); // 메시지 타입 설정
-        message.setContent(messageContent); // 메시지 내용
-        message.setSenderID(userId); // 보내는 사람의 ID
-        message.setRoomId(roomId); // 채팅방 ID 설정
+        message.setType(MessageType.CHAT);
+        message.setContent(messageContent);
+        message.setSenderID(userId);
+        message.setRoomId(roomId);
         return messageRepository.save(message);
     }
 
