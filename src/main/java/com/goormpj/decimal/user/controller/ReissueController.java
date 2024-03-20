f91826d7798cccd59a4c79d42a409c7229444572
dawoon jung <daewangbali@gmail.com>
2024-03-20 22:58:28 +0900

    refactor: reissue refactoring

--- src/main/java/com/goormpj/decimal/user/controller/ReissueController.java
+++ src/main/java/com/goormpj/decimal/user/controller/ReissueController.java
@@ -1,9 +1,6 @@
 package com.goormpj.decimal.user.controller;
 
-import com.goormpj.decimal.jwt.JWTProvider;
-import com.goormpj.decimal.user.domain.RefreshToken;
-import com.goormpj.decimal.user.repository.RefreshTokenRepository;
-import io.jsonwebtoken.ExpiredJwtException;
+import com.goormpj.decimal.user.service.ReissueService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -15,57 +12,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/api")
 public class ReissueController {
 
-    private final JWTProvider jwtProvider;
-
-    private final RefreshTokenRepository refreshTokenRepository;
+    private final ReissueService reissueService;
 
     @PostMapping("/reissue")
     public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response) {
         //get refresh_token from cookies
         String refreshToken = getRefreshToken(request);
 
-        if (refreshToken == null) {
-            return new ResponseEntity<>("refresh_token null", HttpStatus.BAD_REQUEST);
+        String[] renewTokens = reissueService.renewRefreshToken(refreshToken);
+        if(renewTokens.length == 0){
+            return new ResponseEntity<>("refresh_token is null OR invalid refresh_token", HttpStatus.BAD_REQUEST);
         }
 
-        //expired check
-        try {
-            jwtProvider.isExpired(refreshToken);
-        } catch (ExpiredJwtException e) {
-            return new ResponseEntity<>("refresh_token expired", HttpStatus.BAD_REQUEST);
-        }
-
-        // 토큰이 refresh인지 확인 (발급시 페이로드에 명시)
-        String category = jwtProvider.getCategory(refreshToken);
-
-        if (!category.equals("refresh_token")) {
-            return new ResponseEntity<>("invalid refresh_token", HttpStatus.BAD_REQUEST);
-        }
-
-        //DB에 저장되어 있는지 확인
-        Boolean isExist = refreshTokenRepository.existsByRefreshToken(refreshToken);
-        if (!isExist) {
-            return new ResponseEntity<>("invalid refresh_token", HttpStatus.BAD_REQUEST);
-        }
-
-        String memberId = jwtProvider.getMemberId(refreshToken);
-        String role = jwtProvider.getRole(refreshToken);
-
-        String newAccessToken = jwtProvider.createJwt("access_token", memberId, role, 600000L);
-        String newRefreshToken = jwtProvider.createJwt("refresh_token", memberId, role, 86400000L);
-
-        //DB에 기존의 Refresh 토큰 삭제 후 새 Refresh 토큰 저장
-        refreshTokenRepository.deleteByRefreshToken(refreshToken);
-        addRefreshToken(memberId, newRefreshToken, 86400000L);
+        String newAccessToken = renewTokens[0];
+        String newRefreshToken = renewTokens[1];
 
         response.setHeader("access_token", newAccessToken);
         response.addCookie(createCookie("refresh_token", newRefreshToken));
@@ -92,16 +58,6 @@ public class ReissueController {
         cookie.setHttpOnly(true);
         cookie.setPath("/");
         return cookie;
-    }
-
-    private void addRefreshToken(String memberId, String refresh, Long expiredMs) {
-        Date date = new Date(System.currentTimeMillis() + expiredMs);
-        RefreshToken refreshToken = RefreshToken.builder()
-                .memberId(memberId)
-                .refreshToken(refresh)
-                .expirationDate(date)
-                .build();
-        refreshTokenRepository.save(refreshToken);
     }
 
 }
