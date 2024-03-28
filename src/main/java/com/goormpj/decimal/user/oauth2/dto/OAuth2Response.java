d36770e07c7c848cf2e5385e6ec3262870d5fc10
dawoon jung <daewangbali@gmail.com>
2024-03-28 10:53:09 +0900

    feat: 소셜로그인 구현

---
+++ src/main/java/com/goormpj/decimal/user/oauth2/dto/OAuth2Response.java
@@ -0,0 +1,10 @@
+package com.goormpj.decimal.user.oauth2.dto;
+
+public interface OAuth2Response {
+
+    String getProvider();
+    String getProviderId();
+    String getEmail();
+    String getName();
+
+}
